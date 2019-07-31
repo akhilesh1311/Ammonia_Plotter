c09d70122b43a37ff6f297c1b95b1a2b484f1eb1
akhilesh1311 <akhilesh.maha@gmail.com>
2019-07-30 21:41:34 -0400

    Removed unnecessary comments

--- wrapper.R
+++ wrapper.R
@@ -2,7 +2,8 @@
 # Please change this: -----------------------------------------------------
 
 # Directory where all the code files are located
-shiny_app_dir <<- "/home/akhil/Documents/winlab/code/ammonia_plotter/"
+# shiny_app_dir <<- "/home/akhil/Documents/winlab/code/ammonia_plotter/"
+shiny_app_dir <<- "E:/c/Downloads/attachments"
 
 # List of directory/files to load as given below for example. Files have end in .csv only.
 data_dir <<- list(
@@ -10,7 +11,9 @@ data_dir <<- list(
   # "/home/akhil/Documents/winlab/controlled_env/Analysis_11-11-2018",
   # "/home/akhil/Documents/winlab/controlled_env/short_exponential_analysis",
   # "/home/akhil/Documents/winlab/controlled_env/Analysis_26_Nov/20181127_103609.csv"
-  "/home/akhil/Downloads/20190521_180247.csv"
+  # "/home/akhil/Downloads/20190521_180247.csv"
+  "E:/c/Downloads/20190521_180247.csv"
+  # "E:/c/Downloads/Plot_Ammonia_4-11-2019-20190627T155211Z-001/Plot_Ammonia_4-11-2019"
   )
 
 # Plot type, can be: linePoint, line, point
@@ -36,17 +39,3 @@ shiny::onStop(function(){
   remove(list = ls())
 })
 shiny::runApp(appDir = shiny_app_dir, launch.browser = TRUE)
-
-
-# Install one more package
-# install.package("gginnards")
-# Update all packages:
-# update.packages(ask = FALSE)
-# OR
-# package_df <- as.data.frame(installed.packages("/home/akhil/R/x86_64-pc-linux-gnu-library/3.4"))
-# package_list <- as.character(package_df$Package)
-# install.packages(package_list)
-
-
-# install.packages("scales")
-# library(scales)
